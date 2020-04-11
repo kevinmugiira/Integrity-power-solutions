2ba22f5abd59c9c748a718068a2f08610c627d49
kevinmugiira <kevinm5740@aol.com>
2020-04-11 13:24:57 +0300

    Initial commit

---
+++ resources/views/gallery.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+
+
+
+
+@section('content')
+
+
+
+
+    @endsection
---
+++ resources/views/projects.blade.php
@@ -0,0 +1,107 @@
+@extends('master')
+
+
+
+@section('content')
+
+    <!--Gallery-->
+    <section class="gallery py-md-5">
+        <div class="container py-4 mt-2">
+            <h3 class="tittle text-center mb-3">Projects</h3>
+            <p class="tit text-center mx-auto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius eum inventore consectetur dolorum, voluptatum possimus temporibus vel ab, nesciunt quod!</p>
+            <div class="row agile_gallery_grids w3-agile demo pt-4 mt-md-5">
+                <div class="col-md-4 gal-sec">
+                    <div class="gallery-grid1">
+                        <a title="Attractive home purchase offers." href="{{asset('asset/images/g1.jpg')}}">
+                            <img src="{{asset('asset/images/y.jpeg')}}" alt=" " class="img-responsive" />
+                            <div class="p-mask">
+                                <h4>Integrity Power Solutions</h4>
+                                <p>Industry</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="gallery-grid1">
+                        <a title="Attractive home purchase offers." href="{{asset('asset/images/g2.jpg')}}">
+                            <img src="{{asset('asset/images/y.jpeg')}}" alt=" " class="img-responsive" />
+                            <div class="p-mask">
+                                <h4>Integrity Power Solutions</h4>
+                                <p>Industry</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="gallery-grid1">
+                        <a title="Attractive home purchase offers." href="{{asset('asset/images/g3.jpg')}}">
+                            <img src="{{asset('asset/images/y.jpeg')}}" alt=" " class="img-responsive" />
+                            <div class="p-mask">
+                                <h4>Integrity Power Solutions</h4>
+                                <p>Industry</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4 gal-sec">
+                    <div class="gallery-grid1">
+                        <a title="Attractive home purchase offers." href="{{asset('asset/images/g4.jpg')}}">
+                            <img src="{{asset('asset/images/y.jpeg')}}" alt=" " class="img-responsive" />
+                            <div class="p-mask">
+                                <h4>Integrity Power Solutions</h4>
+                                <p>Industry</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="gallery-grid1">
+                        <a title="Attractive home purchase offers." href="{{asset('asset/images/g5.jpg')}}">
+                            <img src="{{asset('asset/images/y.jpeg')}}" alt=" " class="img-responsive" />
+                            <div class="p-mask">
+                                <h4>Integrity Power Solutions</h4>
+                                <p>Industry</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="gallery-grid1">
+                        <a title="Attractive home purchase offers." href="{{asset('asset/images/g6.jpg')}}">
+                            <img src="{{asset('asset/images/y.jpeg')}}" alt=" " class="img-responsive" />
+                            <div class="p-mask">
+                                <h4>Integrity Power Solutions</h4>
+                                <p>Industry</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4 gal-sec">
+                    <div class="gallery-grid1">
+                        <a title="Attractive home purchase offers." href="{{asset('asset/images/g7.jpg')}}">
+                            <img src="{{asset('asset/images/y.jpeg')}}" alt=" " class="img-responsive" />
+                            <div class="p-mask">
+                                <h4>Integrity Power Solutions</h4>
+                                <p>Industry</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="gallery-grid1">
+                        <a title="Attractive home purchase offers." href="{{asset('asset/images/g8.jpg')}}">
+                            <img src="{{asset('asset/images/y.jpeg')}}" alt=" " class="img-responsive" />
+                            <div class="p-mask">
+                                <h4>Integrity Power Solutions</h4>
+                                <p>Industry</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="gallery-grid1">
+                        <a title="Attractive home purchase offers." href="{{asset('asset/images/g9.jpg')}}">
+                            <img src="{{asset('asset/images/y.jpeg')}}" alt=" " class="img-responsive" />
+                            <div class="p-mask">
+                                <h4>Integrity Power Solutions</h4>
+                                <p>Industry</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--//Gallery-->
+
+
+
+@endsection
